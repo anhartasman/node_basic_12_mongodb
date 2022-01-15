fea572a7214ae0f6e5087e7272d313656b0b07a4
Anhar Tasman <anhar_tasman@yahoo.com>
2022-01-15 17:49:21 +0700

    180. Installing the MongoDB Driver

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # node_basic_12_mongodb
+
 Mempelajari mongo db di node js
+
+## 180. Installing the MongoDB Driver
+
+### Intro
+
+Menginstall driver mongodb
+
+### command
+
+```
+npm install --save mongodb
+```
--- util/database.js
+++ util/database.js
@@ -1,7 +1,14 @@
-const Sequelize = require("sequelize");
-
-const sequelize = new Sequelize("nodecomplete", "root", "12345678", {
-  dialect: "mysql",
-  host: "localhost",
-});
-module.exports = sequelize;
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    "mongodb+srv://admin:ph8HjaDfuRZBuLh@cluster0.aay6z.mongodb.net/Cluster0?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+    .then((result) => {
+      console.log("Connected!");
+      callback(result);
+    })
+    .catch((err) => console.log(err));
+};
+module.exports = mongoConnect;
